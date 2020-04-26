53f8a3a8fbd45cf87229f33a2efb713cdb73a6b2
haruyasu <harukun2002@gmail.com>
2020-04-26 10:39:08 +0900

    :rocket: 完成

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    re_path('', views.IndexView.as_view(), name='entry-point'),
 ]
